4e2b93d60a81da19c40db04b4357df66d5339c41
Resto U <resto-u@example.invalid>
2026-08-30 04:40:00 +0000

    wip

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@
 - Ajout de la tarte aux pommes
 - Ajout de la mousse au chocolat
 - Ajout de la salade de fruits
+- Ajout du curry de pois chiches, plat vegetarein
 
 ## [0.1.0] — Menu de base
 - Ajout de la salade de lentilles
--- Menu.java
+++ Menu.java
@@ -15,6 +15,7 @@ public final class Menu {
         new Plat("Œuf mayonnaise", Categorie.ENTREE, 110, true),
         new Plat("Terrine de campagne", Categorie.ENTREE, 150, false),
         new Plat("Steak haché frites", Categorie.PLAT, 390, false),
+        new Plat("Curry de pois chiches", Categorie.PLAT, 300, true),
         new Plat("Yaourt nature", Categorie.DESSERT, 95, true),
         new Plat("Tarte aux pommes", Categorie.DESSERT, 130, true),
         new Plat("Mousse au chocolat", Categorie.DESSERT, 120, true),
